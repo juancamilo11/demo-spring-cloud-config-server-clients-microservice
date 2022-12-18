4cf987ae532415fd0cb0e523cad40ca9ad3d5c5f
Juan Camilo Cardona Calderón <juan.cardona69@udea.edu.co>
2022-12-17 22:54:10 -0500

    Fixed issue

--- src/main/java/dev/j3c/clientsmicroservice/DemoConfigServerClientApplication.java
+++ src/main/java/dev/j3c/clientsmicroservice/DemoConfigServerClientApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@SpringBootApplicationgit
+@SpringBootApplication
 @EnableConfigurationProperties
 public class DemoConfigServerClientApplication {
 
