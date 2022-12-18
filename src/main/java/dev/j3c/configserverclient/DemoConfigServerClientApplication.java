bb4ffe8f798f72ebd92b94fa0e1c204561add61b
Juan Camilo Cardona Calderón <juan.cardona69@udea.edu.co>
2022-12-17 21:26:34 -0500

    Base commit

---
+++ src/main/java/dev/j3c/configserverclient/DemoConfigServerClientApplication.java
@@ -0,0 +1,13 @@
+package dev.j3c.configserverclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoConfigServerClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoConfigServerClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/j3c/configserverclient/DemoConfigServerClientApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.j3c.configserverclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoConfigServerClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
